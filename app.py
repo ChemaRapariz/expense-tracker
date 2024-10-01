a0bd68e0e0e635e41f4ba0e442d0192c798e58fe
Jose Maria Rapariz <josemariarapariz@gmail.com>
2024-10-01 14:47:21 +0200

    Implement filesystem session tracking

--- app.py
+++ app.py
@@ -2,23 +2,28 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
+import os
 
 # from helpers import apology, login_required, usd
 
 # Configure application
 app = Flask(__name__)
 
+# Enable debug mode here
+if __name__ == "__main__":
+    app.run(debug=True)  
+
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = os.path.join(os.getcwd(), 'flask_sessions')
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_USE_SIGNED_COOKIE'] = False
+Session(app)
+
 # Connect to the SQLite database
 connect = sqlite3.connect("expenses.db")
 cursor = connect.cursor()
 
-# Configure session to use filesystem (instead of signed cookies)
-# app.config["SESSION PERMANENT"] = False 
-# app.config["SESSION TYPE"] = "filesystem"
-# Session(app)
-
-
 @app.route('/')
 def index():
-    "Testing the local server and phpLiteAdmin"
+    # Testing the local server and phpLiteAdmin
     return render_template("index.html")
---
+++ helpers.py
@@ -0,0 +1,3 @@
+from functools import wraps
+from flask import g, request, redirect, url_for
+
